9492e30b8cc7952aa32f5097629316220c53bf24
Ruslan Popov <rad@kitchen.(none)>
2009-04-08 00:02:06 +0400

    Удаление ненужного.

--- djangobook/patches/0809041003.sql
+++
@@ -1,2 +0,0 @@
--- добавление поля для модуля жалоб
-alter table djangobook_claims add column email varchar(75) not null after url;
--- djangobook/patches/0901231908.sql
+++
@@ -1,2 +0,0 @@
--- добавление поля для модуля жалоб
-alter table djangobook_claims add column notify bool not null after email;
