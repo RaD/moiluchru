30342b4c61f979f24ee47fe208dea8d18412f6e6
Ruslan Popov <rad@kitchen.(none)>
2009-04-08 00:01:49 +0400

    Удаление ненужного.

--- auth_openid/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8
-
-from django.contrib import admin
-from cargo.auth_openid.models import OpenID
-
-class OpenIDAdmin(admin.ModelAdmin):
-    list_display = ['user', 'url']
-
-admin.site.register(OpenID, OpenIDAdmin)
--- auth_openid/config.mk
+++
@@ -1,2 +0,0 @@
-
-APPNAME=auth_openid
