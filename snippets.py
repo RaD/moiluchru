77c11a3d09e061625bdc801374fb2f542e703100
Ruslan <rad@bk-rpopov.service.jet.msk.su>
2009-04-15 10:33:03 +0400

    Новый декоратор ajax_processor для выдачи JSON в JS.

--- snippets.py
+++ snippets.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # http://markeev.labwr.ru/2008/07/django.html
 
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
-
 from django.template import RequestContext
+from django.utils import simplejson
 
 def render_to(template, processor):
     """ http://www.djangosnippets.org/snippets/821/
@@ -37,4 +38,20 @@ def paged(func):
             kwargs['page'] = 1
         return func(request, *args, **kwargs)
     return wrapper
+
+def ajax_processor(form_object):
+    def processor(func):
+        def wrapper(request, *args, **kwargs):
+            if request.method == 'POST':
+                form = form_object(request.POST)
+                if form.is_valid():
+                    result = func(request, form, *args, **kwargs)
+                else:
+                    result = {'code': '301', 'desc': 'form is not valid'}
+            else:
+                result = {'code': '401', 'desc': 'it must be POST'}
+            json = simplejson.dumps(result)
+            return HttpResponse(json, mimetype="application/json")
+        return wrapper
+    return processor
 
