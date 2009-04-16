e813ff2d3c5d97d4370374e5499c1ef628ef2bb9
Ruslan <rad@bk-rpopov.service.jet.msk.su>
2009-04-16 11:31:51 +0400

    Не надо архивировать изображения. Надо убирать старые резервные копии.

--- dumps/itempics.sh
+++ dumps/itempics.sh
@@ -3,9 +3,10 @@
 BASE=~/django/moiluchru
 DIR=$BASE/dumps
 PIC=$BASE/media/itempics
-DF=$DIR/`date '+%Y%m%d'`.pics.bz2
+DF=$DIR/`date '+%Y%m%d'`.pics.tar
 
 cd $PIC
-tar cjf $DF `find . -type f`
+tar cf $DF `find . -type f`
 cd -
+find $DIR -name '*.pics.tar' -mtime +6 -delete
 exit 0
