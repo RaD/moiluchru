1f1f0350dc371decb73ecae3b96565f5cede1554
Ruslan Popov <rad@kitchen.(none)>
2009-04-17 22:56:30 +0400

    Поправил отображение меню. Отформатировал javascript.

--- js/shop.js
+++ js/shop.js
@@ -1,34 +1,24 @@
-// jQuery.noConflict();
-// jQuery(document).ready(
-//     function() {
-// 	var t_offset = jQuery("#thumbnail").offset()
-// 	var t_height = jQuery("#thumbnail").height()
-// 	jQuery("#item-info-widget").css("top", t_offset.top + t_height - 50)
-//                                    .css("left", t_offset.left - 10)
-//                                    .show();
-//     }
-// );
 
 // Отображение формы подтверждения
 
 function show_form(item_id, item_title) {
-    var widget = jQuery('#widget-addtocart');
+    var widget = $('#widget-addtocart');
     if (! widget) return;
     
-  var apply_func = function() {
-    jQuery.post('/shop/add/',
-		{ item: item_id,
-		  count: jQuery("#widget_item_quantity").val() },
-		function(json) {
-		  if (json['code'] == 200) {
-		    update_cart(json['cart_count'], json['cart_price']);
-		  } else {
-		    alert(json['code'] + ': ' + json['desc']);
-		  }
-		}, 'json' );
-    // скрываем
-    widget.hide();
-  }
+    var apply_func = function() {
+	$.post('/shop/add/',
+	       { item: item_id,
+		 count: $('#widget_item_quantity').val() },
+	       function(json) {
+		   if (json['code'] == 200) {
+		       update_cart(json['cart_count'], json['cart_price']);
+		   } else {
+		       alert(json['code'] + ': ' + json['desc']);
+		   }
+	       }, 'json' );
+	// скрываем
+	widget.hide();
+    }
 
     var cancel_func = function() {
 	// скрываем
@@ -64,40 +54,40 @@ function show_form(item_id, item_title) {
     
     parent.document.onkeypress = function(e) { return onkeypress(e); }
     
-    jQuery("body").append(widget);
-  jQuery("#widget_apply").click(apply_func);
-  jQuery("#widget_cancel").click(cancel_func);
-  widget.show();
+    $('body').append(widget);
+    $("#widget_apply").click(apply_func);
+    $("#widget_cancel").click(cancel_func);
+    widget.show();
     
     // позиционирование на середине экрана
     widget.css("top", (self.innerHeight / 2 - widget.height() / 2 + self.pageYOffset) + 'px');
     widget.css("left", (self.innerWidth / 2 - widget.width() / 2) + 'px');
     
-    jQuery("widget_item_quantity").focus();
+    $("widget_item_quantity").focus();
 }
 
 function update_cart(count, price) {
-  jQuery("#cart_count").html(count);
-  jQuery("#cart_price").html(price);
-  if (count == 0)
-    jQuery("#clean_button").hide();
-  else
-    jQuery("#clean_button").show();
+    $('#cart_count').html(count);
+    $('#cart_price').html(price);
+    if (count == 0)
+	$('#clean_button').hide();
+    else
+	$('#clean_button').show();
 }
 
 function clean_cart(url) {
-  jQuery.post('/shop/clean/', {},
-	      function(json) {
-		if (json['code'] == 200) {
-		  update_cart("0", "0.00");
-		  if (url) 
-		    window.setTimeout(function() { 
-					document.location = url; }, 
-				      3000); 
-		} else {
-		  alert(json['code'] + ': ' + json['desc']);
-		}
-	      }, 'json' );
+    $.post('/shop/clean/', {},
+	   function(json) {
+	       if (json['code'] == 200) {
+		   update_cart("0", "0.00");
+		   if (url) 
+		       window.setTimeout(function() { 
+			   document.location = url; }, 
+					 3000); 
+	       } else {
+		   alert(json['code'] + ': ' + json['desc']);
+	       }
+	   }, 'json' );
 }
 
 $.fn.alignCenter = function() {
