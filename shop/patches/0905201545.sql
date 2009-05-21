62e5ef55324085d01a58680c84c481f128591222
Ruslan <rad@fujitsu.(none)>
2009-05-21 12:00:06 +0400

    Заполнение поля last_modification.

--- shop/patches/0905201545.sql
+++ shop/patches/0905201545.sql
@@ -1,2 +1,3 @@
 -- добавил поле для регистрации даты/времени последнего изменения
 alter table shop_item add column `last_modification` datetime not null after reg_date;
+update shop_item set last_modification = reg_date;
