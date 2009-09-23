0b7cc5c66538c06b06955b622234b489872cedbf
Ruslan <ruslan.popov@gmail.com>
2009-09-23 12:53:43 +0400

    Добавил команду manage.py import_template для визажиста.

---
+++ visagiste/management/commands/import_templates.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Команда manage.py для импортирования шаблонов в модель.
+
+from django.conf import settings
+from django.core.management.base import NoArgsCommand
+
+from visagiste.models import Template
+
+import os, glob
+
+class Command(NoArgsCommand):
+    help = "Populates Visagiste.Template model with the content of templates. Useful after installation."
+
+    def handle_noargs(self, **options):
+        print 'WORKS'
+        Template.objects.all().delete()
+        for indir in settings.TEMPLATE_DIRS:
+            for infile in glob.glob(os.path.join(indir, '*.html')):
+                content = open(infile, 'r').read()
+                Template(name=infile, content=content).save()
